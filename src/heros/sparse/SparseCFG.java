d91a21d62b28faf8ca2918fb4a09ee8c5b2f05a5
Kadiray Karakaya <kadiray.k@hotmail.com>
2023-03-27 19:38:37 +0200

    minor

--- src/heros/sparse/SparseCFG.java
+++ src/heros/sparse/SparseCFG.java
@@ -12,7 +12,7 @@ public interface SparseCFG<N, D> {
 
     /**
      * returns next uses after given stmt
-     * can return multiple uses when brancing
+     * can return multiple uses when branching
      *
      * @param n
      * @return
@@ -22,9 +22,5 @@ public interface SparseCFG<N, D> {
     void add(N n);
 
     D getD();
-
-    List<N> getCFG();
-
-    void setCFG(List<N> cfg);
 
 }
--- src/heros/sparse/SparseCFGCache.java
+++ src/heros/sparse/SparseCFGCache.java
@@ -1,10 +1,7 @@
 package heros.sparse;
 
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class SparseCFGCache<M, D> {
 
@@ -66,7 +63,8 @@ public class SparseCFGCache<M, D> {
 
     public static List<SparseCFGQueryStat> getQueryStats(){
         if(INSTANCE==null){
-            throw new RuntimeException("Invalid state");
+            return Collections.emptyList();
+
         }
         return INSTANCE.queryStats;
     }
